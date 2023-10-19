0b77ac961d0e22925fa7e2e15e4c4e11cd84204a
Alina Rubart <rubartalina@gmail.com>
2023-10-19 12:27:42 +0200

    connect API to frontend

--- pages/api/quizResults/index.js
+++ pages/api/quizResults/index.js
@@ -1,12 +1,13 @@
 export default async function handler(request, response) {
   // step 2: backend makes request to external API
-
+  console.log("request.query:", request.query);
   if (request.method === "GET") {
     // make request to API. then send back response.
     // this example does not use any API KEYS.
     // If you need to keep something secret you could do the following. {process.env.YOUR_NAME_OF_THE_ENV_VARIABLE}
+
     const dogResponse = await fetch(
-      "https://api.api-ninjas.com/v1/dogs?barking=5",
+      `https://api.api-ninjas.com/v1/dogs?barking=${request.query.barking}`,
       {
         headers: {
           "X-Api-Key": process.env.DOG_API_KEY,
