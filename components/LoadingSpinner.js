fb27b0b920231a55dd5af76fc370446b006a3d5e
Gregor Haslinger <kontakt@gregorsart.de>
2023-11-04 11:50:53 +0100

    add fade in to loading spinner

--- components/LoadingSpinner.js
+++ components/LoadingSpinner.js
@@ -18,18 +18,22 @@ export default function LoadingSpinner() {
 const customAnimation = keyframes`
     0% {
       transform: rotate(0turn);
+      opacity: 0;
     }
     25% {
       transform: rotate(0.08turn);
     }
     50% {
       transform: rotate(0turn);
+     
     }
     75% {
       transform: rotate(-0.08turn);
+      opacity: 1;
     }
     100% {
       transform: rotate(0turn);
+      opacity: 0;
     }
 `;
 
@@ -37,11 +41,11 @@ const StyledWrapper = styled.section`
   display: flex;
   align-items: center;
   height: 100vh;
-  animation: ${customAnimation} 1500ms;
+  animation: ${customAnimation} 2500ms;
   animation-iteration-count: infinite;
   animation-timing-function: ease-in-out;
 `;
 
 const StyledImage = styled(Image)`
-  margin-top: -4rem;
+  margin-top: -1rem;
 `;
