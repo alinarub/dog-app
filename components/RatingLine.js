857e40a9ecc5a627a43c00c4567b4b1f8043e221
Gregor Haslinger <kontakt@gregorsart.de>
2023-11-06 17:33:51 +0100

    refactor five star rating in rating line component

--- components/RatingLine.js
+++ components/RatingLine.js
@@ -3,28 +3,17 @@ import Image from "next/image";
 
 export default function RatingLine({ characteristic, rating }) {
   let stars = [];
+
   for (let i = 1; i < 6; ++i) {
-    if (i <= +rating) {
-      stars.push(
-        <StyledImage
-          src="/paw-icon-yellow.svg"
-          alt="Icon of a paw"
-          width={15}
-          height={15}
-          blurDataURL="data:..."
-        />
-      );
-    } else {
-      stars.push(
-        <StyledImage
-          src="/paw-icon-gray.svg"
-          alt="Icon of a paw"
-          width={15}
-          height={15}
-          blurDataURL="data:..."
-        />
-      );
-    }
+    stars.push(
+      <StyledImage
+        src={i <= +rating ? "/paw-icon-yellow.svg" : "/paw-icon-gray.svg"}
+        alt="Icon of a paw"
+        width={15}
+        height={15}
+        blurDataURL="data:..."
+      />
+    );
   }
 
   return (
