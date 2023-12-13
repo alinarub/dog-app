4b0f483d8780d4cb810ebc4f0ab3629b9d8a53f9
Achim Bartscht <beemerhh@gmail.com>
2023-12-13 11:19:37 +0100

    add hover effect circle animation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import Image from "next/image";
 import Link from "next/link";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import LinkButton from "@/components/LinkButton";
 import Headline from "@/components/Headline";
 import ImageTextModule from "@/components/ImageTextModule";
@@ -28,19 +28,24 @@ export default function HomePage() {
       </LinkButton>
       <Headline>Dog of the moment</Headline>
       <StyledLink href={`dogs/${randomDog.name}`}>
-        <StyledImage
-          src={randomDog.image_link}
-          alt={`Image of ${randomDog.name}`}
-          width={256}
-          height={171}
-          blurDataURL="data:..."
-          placeholder="blur" // Optional blur-up while loading
-        />
+        <div>
+          <StyledFigure>
+            <StyledImage
+              src={randomDog.image_link}
+              alt={`Image of ${randomDog.name}`}
+              width={256}
+              height={171}
+              blurDataURL="data:..."
+              placeholder="blur" // Optional blur-up while loading
+            />
+          </StyledFigure>
+        </div>
         <StyledParagraph>{randomDog.name}</StyledParagraph>
       </StyledLink>
     </>
   );
 }
+
 const StyledImage = styled(Image)`
   border-radius: var(--borderradius-medium);
   outline: 2px solid var(--primary-color);
@@ -48,13 +53,52 @@ const StyledImage = styled(Image)`
   margin: auto;
   margin-top: 2rem;
 `;
+
 const StyledParagraph = styled.p`
   text-align: center;
   margin-top: 1rem;
   margin-bottom: 2rem;
   color: var(--font-color);
 `;
+
+const circle = keyframes`
+  0% {
+      opacity: 1;
+    }
+    40% {
+      opacity: 1;
+    }
+    100% {
+      width: 200%;
+      height: 200%;
+      opacity: 0;
+    }
+`;
+
 const StyledLink = styled(Link)`
   text-decoration: none;
   color: var(--font-color);
+
+  &:hover figure::before {
+    animation: ${circle} 0.5s;
+  }
+
+  figure::before {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 2;
+    display: block;
+    content: "";
+    width: 0;
+    height: 0;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 100%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+  }
+`;
+
+const StyledFigure = styled.figure`
+  position: relative;
 `;
